bf0bc18f6f36f83285c5879f3d6118f5dbf227c2
Oleg Kozyrev <ovkozyrev@avito.ru>
2023-07-27 19:40:46 +0300

    add grpc example

---
+++ week_1/grpc/cmd/data_size/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/brianvoe/gofakeit"
+	"github.com/golang/protobuf/proto"
+
+	desc "github.com/olezhek28/microservices_course/week_1/grpc/pkg/note_v1"
+)
+
+func main() {
+	session := &desc.NoteInfo{
+		Title:    gofakeit.BeerName(),
+		Context:  gofakeit.IPv4Address(),
+		Author:   gofakeit.Name(),
+		IsPublic: gofakeit.Bool(),
+	}
+
+	dataJson, _ := json.Marshal(session)
+	fmt.Printf("\n\ndataJson len %d byte \n%v\n", len(dataJson), dataJson)
+
+	dataPb, _ := proto.Marshal(session)
+	fmt.Printf("dataPb len %d byte \n%v\n", len(dataPb), dataPb)
+}
