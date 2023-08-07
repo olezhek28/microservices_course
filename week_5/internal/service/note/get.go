5b679a4ffad4347d3a17c99dfc76e1e96e3f5ffc
Oleg Kozyrev <ovkozyrev@avito.ru>
2023-08-07 19:09:03 +0300

    add base tmpl

---
+++ week_5/internal/service/note/get.go
@@ -0,0 +1,16 @@
+package note
+
+import (
+	"context"
+
+	"github.com/olezhek28/microservices_course/week_5/internal/model"
+)
+
+func (s *serv) Get(ctx context.Context, id int64) (*model.Note, error) {
+	note, err := s.noteRepository.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return note, nil
+}
