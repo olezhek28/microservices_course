552f374831c7e53a9e59af9780df886f0756bc5d
Oleg Kozyrev <ovkozyrev@avito.ru>
2023-08-15 20:02:18 +0300

    add trace id in grpc response header

--- week_7/grpc_with_traces/internal/interceptor/server_tracing.go
+++ week_7/grpc_with_traces/internal/interceptor/server_tracing.go
@@ -5,12 +5,27 @@ import (
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
+	"github.com/uber/jaeger-client-go"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
+
+const traceIDKey = "x-trace-id"
 
 func ServerTracingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	span, ctx := opentracing.StartSpanFromContext(ctx, info.FullMethod)
 	defer span.Finish()
+
+	spanContext, ok := span.Context().(jaeger.SpanContext)
+	if ok {
+		ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs(traceIDKey, spanContext.TraceID().String()))
+
+		header := metadata.New(map[string]string{traceIDKey: spanContext.TraceID().String()})
+		err := grpc.SendHeader(ctx, header)
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	res, err := handler(ctx, req)
 	if err != nil {
