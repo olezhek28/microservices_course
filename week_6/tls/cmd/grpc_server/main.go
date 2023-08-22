0552905485eb8c73b58d815e5934d6a0d498d1a8
Oleg Kozyrev <ovkozyrev@avito.ru>
2023-08-22 21:47:44 +0300

    add base project for tls

---
+++ week_6/tls/cmd/grpc_server/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/olezhek28/microservices_course/week_6/tls/pkg/note_v1"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedNoteV1Server
+}
+
+// Get ...
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Note id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		Note: &desc.Note{
+			Id: req.GetId(),
+			Info: &desc.NoteInfo{
+				Title:    gofakeit.BeerName(),
+				Context:  gofakeit.IPv4Address(),
+				Author:   gofakeit.Name(),
+				IsPublic: gofakeit.Bool(),
+			},
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterNoteV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
