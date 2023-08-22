3d536da5bc34de625bee6164690a81b45f72000a
Oleg Kozyrev <ovkozyrev@avito.ru>
2023-08-22 23:00:47 +0300

    add base project for custom errors

---
+++ week_8/custom_errors/cmd/grpc_server/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/olezhek28/microservices_course/week_8/custom_errors/pkg/note_v1"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedNoteV1Server
+}
+
+// Get ...
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	if req.GetId() == 0 {
+		return nil, errors.Errorf("id is empty")
+	}
+
+	return &desc.GetResponse{
+		Note: &desc.Note{
+			Id: req.GetId(),
+			Info: &desc.NoteInfo{
+				Title:    gofakeit.BeerName(),
+				Context:  gofakeit.IPv4Address(),
+				Author:   gofakeit.Name(),
+				IsPublic: gofakeit.Bool(),
+			},
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(
+			grpcMiddleware.ChainUnaryServer(),
+		),
+	)
+	reflection.Register(s)
+	desc.RegisterNoteV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ week_8/custom_errors/go.mod
@@ -0,0 +1,19 @@
+module github.com/olezhek28/microservices_course/week_8/custom_errors
+
+go 1.20
+
+require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	github.com/pkg/errors v0.8.1
+	google.golang.org/grpc v1.57.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+)
