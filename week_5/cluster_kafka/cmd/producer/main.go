10f6ae3bf5da1f8eef8b185eee7650f0e4ad0e94
Oleg Kozyrev <ovkozyrev@avito.ru>
2024-06-14 19:54:07 +0300

    add kafka cluster example

---
+++ week_5/cluster_kafka/cmd/producer/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/IBM/sarama"
+	"github.com/brianvoe/gofakeit"
+)
+
+const (
+	brokerAddress = "localhost:9092, localhost:9093, localhost:9094"
+	topicName     = "test-topic"
+)
+
+func main() {
+	producer, err := newSyncProducer(strings.Split(brokerAddress, ","))
+	if err != nil {
+		log.Fatalf("failed to start producer: %v\n", err.Error())
+	}
+
+	defer func() {
+		if err = producer.Close(); err != nil {
+			log.Fatalf("failed to close producer: %v\n", err.Error())
+		}
+	}()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topicName,
+		Value: sarama.StringEncoder(gofakeit.StreetName()),
+	}
+
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("failed to send message in Kafka: %v\n", err.Error())
+		return
+	}
+
+	log.Printf("message sent to partition %d with offset %d\n", partition, offset)
+}
+
+func newSyncProducer(brokerList []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokerList, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return producer, nil
+}
