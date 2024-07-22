cdd20e11f88d411b8c368b53c000e39183a8d9f6
Oleg Kozyrev <ovkozyrev@avito.ru>
2024-07-22 16:12:33 +0300

    clean up

--- week_5/clean_kafka/internal/repository/note/redis/model/note.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Note struct {
-	ID          int64  `redis:"id"`
-	Title       string `redis:"title"`
-	Content     string `redis:"content"`
-	CreatedAtNs int64  `redis:"created_at"`
-	UpdatedAtNs *int64 `redis:"updated_at"`
-}
